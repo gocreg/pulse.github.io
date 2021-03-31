c53cefe9b4089ed92dbe7df4d9511ccdddb26021
gocreg <jackie121973@gmail.com>
2021-03-31 05:38:50 +0300

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $('.corousel__inner').slick({
         speed: 300,
         slidesToShow: 1,
-        // adaptiveHeight: true,
+        //adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png"></button>',
         responsive: [{
             breakpoint: 992,
             settings: {
-                dots: true,
+                dots: false,
                 arrows: false
             }
         }]
