04bb34eb6e6eb50b7f4098816edd1f577b92b107
gocreg <jackie121973@gmail.com>
2021-04-06 19:22:38 +0300

    validate forms

--- src/js/script.js
+++ src/js/script.js
@@ -20,9 +20,9 @@ $(document).ready(function () {
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    function toggleSlide (item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -30,23 +30,64 @@ $(document).ready(function () {
         });
     };
 
-    toggleSlide ('.catalog-item__link');
-    toggleSlide ('.catalog-item__back');
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 
     //modal
 
-    $('[data-modal=consultation]').on('click', function() {
+    $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('slow');
     });
 
-    $('.modal__close').on('click', function() {
+    $('.modal__close').on('click', function () {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
 
-    $('.price__button').each(function(i) {
-        $(this).on('click', function() {
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); 
+    $('.price__button').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
-        })
-    })
+        });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: {
+                    required: true,
+                    minlength: 6
+                },
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Поле имя обязательно для заполнения",
+                    minlength: jQuery.validator.format("Длина имени должна быть больше 2-ух символов")
+                },
+                phone: {
+                    required: "Поле телефон обязательно для заполнения",
+                    minlength: jQuery.validator.format("Номер телефона должен быть больше 5-ти символов")
+                },
+                email: {
+                    required: "Введите Вашу почту",
+                    email: "Ваша почта должна быть в формате - name@domain.com"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#promo-form');
+    validateForms('#order-form');
+
+    $('input[name=phone').mask("+7 (999) 999-99-99");
+
+    
 });
