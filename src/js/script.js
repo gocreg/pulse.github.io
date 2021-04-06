47eedf6fc2e86bb40f0e0ac229fb8c8363bb78fb
gocreg <jackie121973@gmail.com>
2021-04-06 09:31:28 +0300

    final adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.corousel__inner').slick({
         speed: 300,
         slidesToShow: 1,
-        //adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/chevron-right-solid.png"></button>',
         responsive: [{
@@ -19,8 +19,6 @@ $(document).ready(function () {
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-
-   
 
     function toggleSlide (item) {
         $(item).each(function(i) {
